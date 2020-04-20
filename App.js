8f337ac616f4160af18f96a1531a88a2fcc1a7b2
Nikolassantos <nikolassantoshttp@gmail.com>
2020-04-20 14:44:03 -0300

    Add Commitlint

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View } from 'react-native';
-import Login from './src/screen/initialscreen';
+import Screen from './src/screen/initialscreen';
+import Dashboard from './src/screen/Dashboard';
 
 export default function App() {
-  return <Login />;
+  return <Dashboard />;
 }
